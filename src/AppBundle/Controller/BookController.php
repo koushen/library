ba81547bb9042ea4fedf0b32e71bf8ed948d7992
Alberto <Alberto>
2016-08-02 13:05:10 +0200

    añadimos BookController para la api RESTFUL

---
+++ src/AppBundle/Controller/BookController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alberto
+ * Date: 01/08/2016
+ * Time: 14:19
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BookController
+{
+    /**
+     *
+     * * @Route(
+     *       path     = "/book/{book_id}",
+     *       defaults = { "book" = "1001" }
+     * )
+     */
+    public function indexAction($book_id)
+    {
+        return new Response(
+            '<html><body>Estas buscando informacion sobre el libro: '.$book_id.'</body></html>'
+        );
+    }
+}
